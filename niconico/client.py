657c1d0907a0849a10eb7346c2f995d333ca7f8b
tasuren <tasuren_0618@yahoo.co.jp>
2021-12-20 20:25:12 +0900

    update: requestの関数をClientに移動した。

--- niconico/client.py
+++ niconico/client.py
@@ -2,10 +2,20 @@
 
 from typing import Optional
 
+import requests
+
 from .video import Client as VideoClient
 from .cookies import Cookies
 
 
 class NicoNico:
     def __init__(self, cookies: Optional[Cookies] = None):
-        self.video = VideoClient(cookies)
+        self.video = VideoClient()
+        self.cookies = cookies
+
+    def request(self, *args, **kwargs) -> requests.Response:
+        kwargs["cookies"] = self.cookies
+        response = requests.request(*args, **kwargs)
+        if self.cookies is None:
+            self.cookies = Cookies.guest(response.cookies["nicosid"])
+        return response
