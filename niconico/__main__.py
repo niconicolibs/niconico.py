80dbe2b60aced5dc95912a69cce3e1db13b4f35b
tasuren <tasuren_0618@yahoo.co.jp>
2022-02-03 21:24:09 +0900

    coding: 作りかけ。

--- niconico/__main__.py
+++ niconico/__main__.py
@@ -5,12 +5,6 @@ import logging
 
 from json import dump
 
-import sys
-if __package__ is None and not hasattr(sys, 'frozen'):
-    import os.path
-    path = os.path.realpath(os.path.abspath(__file__))
-    sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
-
 from niconico import NicoNico, Cookies
 
 
