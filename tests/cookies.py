19499b5e9041e40036eb7ecf5bed256114525512
Negima1072 <ngm1072@gmail.com>
2024-08-07 20:11:19 +0900

    init: v2

--- tests/cookies.py
+++
@@ -1,6 +0,0 @@
-
-import path_magic
-from niconico import Cookies
-
-
-print(list(Cookies.from_file("cookies.txt").keys()))
--- tests/video_viewer.py
+++
@@ -1,8 +0,0 @@
-
-import path_magic
-from niconico import NicoNico, Cookies
-
-
-client = NicoNico(Cookies.from_file("cookies.txt"))
-video = client.video.get_video("https://www.nicovideo.jp/watch/sm37658498")
-print(f"好き：{video.video.viewer.like.isLiked}")
